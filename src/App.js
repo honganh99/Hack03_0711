738c0e63bc165104bcde1d580086267b5cc73f9a
honganh99 <nikkochan@MacBook-Pro-cua-Co-En.local>
2023-11-07 15:44:51 +0700

    hanh

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import WordCounter from "./component/WordCounter";
+
+function App() {
+  return (
+    <div className="App">
+      <WordCounter />
+    </div>
+  );
+}
+
+export default App;
